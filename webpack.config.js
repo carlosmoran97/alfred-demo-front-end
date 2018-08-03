2302f10dd1883ff5cd623560d758d11ec3b66ae3
Carlos Morán <moran_krlos@gotmail.com>
2018-08-02 23:28:26 -0600

    setup prodution webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,57 @@
 const path = require('path');
-
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const publicDir = path.join(__dirname, 'public');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: publicDir,
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        },{
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        },{
-            test: /\.(png|jp(e*)g|svg)$/,  
-            use: [{
-                loader: 'url-loader',
-                options: { 
-                    limit: 8000, // Convert images < 8kb to base64 strings
-                    name: 'images/[hash]-[name].[ext]'
-                } 
+module.exports = (env) => {
+
+    const isProd = env === 'production';
+    const CSSExtract = new ExtractTextPlugin('style.css');
+    return {
+        entry: './src/app.js',
+        output: {
+            path: publicDir,
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            },{
+                test: /\.s?css$/,
+                use: CSSExtract.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                'sourceMap': true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options:{
+                                'sourceMap': true
+                            }
+                        }
+                    ]
+                })
+            },{
+                test: /\.(png|jp(e*)g|svg)$/,  
+                use: [{
+                    loader: 'url-loader',
+                    options: { 
+                        limit: 8000, // Convert images < 8kb to base64 strings
+                        name: 'images/[hash]-[name].[ext]'
+                    } 
+                }]
             }]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: publicDir
-    },
-    mode: 'development'
-}
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProd? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: publicDir
+        }
+    }
+};
