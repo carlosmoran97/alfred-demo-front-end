420fe9ef356f736865d76c3edeb35e1e6216eb4a
Carlos Moran <carlosmoran.97cr@gmail.com>
2018-08-06 15:12:00 -0600

    connecting to api

--- src/components/AlfredDemo.js
+++ src/components/AlfredDemo.js
@@ -4,29 +4,29 @@ import Action from './Action';
 import Notifications from './Notifcations';
 import logo from '../images/alfred.png';
 import profilePic from '../images/profilePic.png';
-import Client from '../models/Client';
-import Notification from '../models/Notification';
+import $ from 'jquery';
 
-// const client = new Client(1, 'Carlos', 'male');
-// const notification = new Notification(1,"message", client, new Date());
-// var notifications = [
-//     notification
-// ];
-// localStorage.setItem('notifications', JSON.stringify(notifications));
 export default class AlfreDemo extends React.Component{
     state = {
         notifications: []
     };
     componentDidMount(){
         // fetching notifications
-        const json = JSON.parse(localStorage.getItem('notifications'));
-        if(json){
-            if(json.length > 0){
-                this.setState(() => ({
-                    notifications: json
-                }));
+        $.ajax({
+            url: 'https://alfred-demo-api.herokuapp.com/api/notifications/',
+            method: 'GET',
+            dataType: 'JSON',
+            success: (data) => {
+                console.log(data);
             }
-        }
+        });
+        // if(json){
+        //     if(json.length > 0){
+        //         this.setState(() => ({
+        //             notifications: json
+        //         }));
+        //     }
+        // }
     };
     render(){
         return(
--- src/models/Client.js
+++
@@ -1,7 +0,0 @@
-export default class Client{
-    constructor(id, name = 'Anonymous', gender = 'Unknown'){
-        this.id = id;
-        this.name = name;
-        this.gender = gender;
-    }
-}
